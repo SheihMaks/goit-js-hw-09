609629cba792ee97c90a155b809244c15f667c29
Maksym Padalka <sheihmaks@gmail.com>
2022-05-12 01:20:07 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const refs = {
+  body: document.querySelector('body'),
+  btnStart: document.querySelector('[data-start]'),
+  btnStop: document.querySelector('[data-stop]'),
+};
 
+let changeColorAtInterval = null;
+
+refs.btnStop.setAttribute('disabled', true);
+
+const onChangeColorAtInterval = () => {
+  refs.body.style.backgroundColor = getRandomHexColor();
+};
+
+const onClickBtnStart = ev => {
+  changeColorAtInterval = setInterval(onChangeColorAtInterval, 1000);
+  refs.btnStop.disabled = false;
+  refs.btnStart.setAttribute('disabled', true);
+};
+
+const onClickBtnStop = ev => {
+  clearInterval(changeColorAtInterval);
+  refs.btnStart.disabled = false;
+  refs.btnStop.disabled = true;
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+refs.btnStart.addEventListener('click', onClickBtnStart);
+refs.btnStop.addEventListener('click', onClickBtnStop);
