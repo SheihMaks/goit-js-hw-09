a98e91071d1e6dec1a0e190deee9e86d26db416e
Maksym Padalka <sheihmaks@gmail.com>
2022-05-17 00:56:52 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,30 @@
+import Notiflix from 'notiflix';
+const formRef = document.querySelector('.form');
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      resolve(console.log(`✅ Fulfilled promise ${position} in ${delay}ms`));
+    } else {
+      reject(console.log(`❌ Rejected promise ${position} in ${delay}ms`));
+    }
+  });
 }
+
+const onSubmit = event => {
+  event.preventDefault();
+  const delay = event.currentTarget.elements.delay.value;
+  const step = event.currentTarget.elements.step.value;
+  const amount = event.currentTarget.elements.amount.value;
+
+  for (let i = 0; i < amount; i += 1) {
+    setTimeout(() => {
+      createPromise(i + 1, +delay + i * +step)
+        .then(message => Notiflix.Notify.success(message))
+        .catch(message => Notiflix.Notify.failure(message));
+    }, +delay + i * +step);
+  }
+  event.currentTarget.reset();
+};
+formRef.addEventListener('submit', onSubmit);
